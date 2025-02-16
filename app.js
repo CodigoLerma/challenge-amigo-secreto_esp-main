0abab941e4f7cd9391d4b357658dae1f30376818
Codigo Lerma <diego0303lc@gmail.com>
2025-02-16 01:41:36 -0600

    Se agrego la funcion agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = []; //almacenara los nombres agregados
+
+
+function agregarAmigo() {
+    //obtener el nombre escrito por el usuario
+    let nombreAmigo = document.getElementById("amigo"); 
+    let nombre = nombreAmigo.value.trim();
+
+    // comprobar que nombre no este vacio
+    if (nombre) {
+        nombres.push(nombre);
+        nombreAmigo.value = ""
+        console.log(nombres)
+    }
+
+}
