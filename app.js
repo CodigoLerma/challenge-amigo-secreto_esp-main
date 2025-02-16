676ac9db318a40a779840dc2bb2cef9b4e32ed55
Codigo Lerma <diego0303lc@gmail.com>
2025-02-16 02:04:43 -0600

    Se grego la funccion actualizar lista que mostrara los nombres

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ let nombres = []; //almacenara los nombres agregados
 function agregarAmigo() {
     //obtener el nombre escrito por el usuario
     let nombreAmigo = document.getElementById("amigo"); 
-    let nombre = nombreAmigo.value.trim();
+    let nombre = nombreAmigo.value.trim(); 
 
-    // comprobar que nombre no este vacio
+    // comprobar que el input de nombre no este vacio
     if (nombre) {
-        nombres.push(nombre);
-        nombreAmigo.value = ""
-        console.log(nombres)
+        nombres.push(nombre); // agrega los nombres al array
+        actualizarLista()
+        nombreAmigo.value = "";
     }
 
+}
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+
+    // une los nombres en el array con saltos de linea. se mostrara vacio si el array esta vacio
+    lista.innerHTML = nombres.join("<br>"); 
+    // ejemplo: "Diego<br>Sofia<br>"
 }
